cce520cfca14d2a7b8e3dd38ac95ec57634fe959
garrett price and stephen newkirk <stephen@>
2016-10-17 14:01:58 -0700

    Create basic to-do with ability to edit.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,10 +4,43 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   <div class="container">
+    <div *ngFor="let currentTask of tasks">
+      <h3>{{ currentTask.description }}</h3>
+      <button (click)="showDetails(currentTask)">Edit</button>
+    </div>
+    <div *ngIf="selectedTask">
+      <h1>Edit Task</h1>
+      <div>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+      </div>
+      <div>
+        <label>Enter Task ID:</label>
+        <input [(ngModel)]="selectedTask.id">
+        <button (click)="finishedEditing()">Done</button>
+      </div>
+    </div>
   </div>
   `
 })
 
 export class AppComponent {
+  public tasks: Task[] = [
+    new Task("Create To-Do list app.", 0),
+    new Task("Learn Kung Fu.", 1),
+    new Task("Rewatch all the Lord of the Rings movies.",2),
+    new Task("Do the laundry.", 3)
+  ];
+  selectedTask: Task = null;
+  showDetails(clickedTask: Task) {
+      this.selectedTask = clickedTask;
+  }
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+}
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number){ }
 }
